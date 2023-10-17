4e6d9a5cf1b4b4e5dbedc1c6653c323826329eaa
Sheryf Habib <sheryf.534@gmail.com>
2023-10-17 14:43:29 +0100

    added logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
-
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg"
 
 function App() {
   return (
-    <div className="main-section"></div>
+    <div className="main-section">
+      <LogoContainer />
+      <MapView />
+      <SearchContainer />
+    </div>
   );
 }
+
+const LogoContainer = () => {
+  return (
+    <div className="logo-container">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+const MapView = () => {
+  return <div className="map-container"></div>;
+};
+
+const SearchContainer = () => {
+  return <div className="search-container"></div>;
+};
 
 export default App;
