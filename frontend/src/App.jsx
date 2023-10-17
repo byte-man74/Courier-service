6d606bbc8142f9f9ec8b8725542e3af7e54b47d5
Sheryf Habib <sheryf.534@gmail.com>
2023-10-17 16:31:52 +0100

    progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
-import logo from "./logo.svg"
+import logo from "./logo.svg";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import React from "react";
 
 function App() {
   return (
@@ -21,23 +23,98 @@ const LogoContainer = () => {
 };
 
 const MapView = () => {
-  return <div className="map-container"></div>;
+  return (
+    <div className="map-container">
+      <MyMapComponent />
+      <div className="map-overlay"></div>
+    </div>
+  );
+};
+
+const MyMapComponent = () => {
+  const containerStyle = {
+    width: "100%",
+    height: "100%",
+  };
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523,
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBP6dRPyFctOHxbZMli7z7kmdxqH2GwPgI",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 };
 
 const SearchContainer = () => {
-  return <div className="search-container">
-    <div className="search-container-top"></div>
-    <input type="text" className="search-container-input" placeholder="Search fueling station by name"/>
-  </div>;
+  return (
+    <div className="search-container">
+      <div className="search-container-top"></div>
+      <input
+        type="text"
+        className="search-container-input"
+        placeholder="Search fueling station by name"
+      />
+
+      {/* state selection */}
+      <select
+        // value={selectedOption}
+        className="dropdown-list"
+        // onChange={handleOptionChange}
+      >
+        <option value="">--Select a state--</option>
+      </select>
+
+      {/* state selection */}
+      <select
+        // value={selectedOption}
+        className="dropdown-list"
+        // onChange={handleOptionChange}
+      >
+        <option value="">--Select a local government--</option>
+      </select>
+
+      {/* buttton container  */}
+      <div className="button">
+        <p style={{ fontSize: 14, fontWeight: "bold", color: "" }}>Search station</p>
+      </div>
+    </div>
+  );
 };
 
-
 const ResultSummaryContainer = () => {
-  return (
-    <div className="result-summary-container"></div>
-  )
-}
-
-
+  return <div className="result-summary-container"></div>;
+};
 
 export default App;
