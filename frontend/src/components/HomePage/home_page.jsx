39c3ec5ddd98b345d735c36d8343945cb7ddc4a5
Sheryf Habib <sheryf.534@gmail.com>
2023-10-18 07:11:50 +0100

    get location works

--- frontend/src/components/HomePage/home_page.jsx
+++ frontend/src/components/HomePage/home_page.jsx
@@ -3,10 +3,14 @@ import { LogoContainer } from './logo'
 import { MapView } from './maps'
 import { SearchContainer } from './search_container'
 import { ResultSummaryContainer } from './result_summary'
+import { useState } from 'react'
 
 const HomePage = () => {
   return (
     <div className="main-section">
+        {/* <div className="main-overlay">
+
+        </div> */}
     <LogoContainer />
     <MapView />
     <SearchContainer />
--- frontend/src/components/HomePage/result_summary.jsx
+++ frontend/src/components/HomePage/result_summary.jsx
@@ -8,6 +8,14 @@ export const ResultSummaryContainer = () => {
           <FrontPagiationButton />
         </div>
       </div>
+      <div className="result-summary-body">
+        <ResultBody />
+        <ResultBody />
+        <ResultBody />
+        <ResultBody />
+        <ResultBody />
+        <ResultBody />
+      </div>
     </div>
   );
 };
@@ -65,4 +73,35 @@ const FrontPagiationButton = () => {
       </svg>
     </div>
   );
+};
+
+const FuelIconSvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="33"
+      height="33"
+      viewBox="0 0 26 26"
+      fill="none"
+    >
+      <path
+        d="M3.25 23.8334H16.25M4.33333 9.75008H15.1667M15.1667 23.8334V4.33341C15.1667 3.75878 14.9384 3.20768 14.5321 2.80135C14.1257 2.39502 13.5746 2.16675 13 2.16675H6.5C5.92536 2.16675 5.37426 2.39502 4.96794 2.80135C4.56161 3.20768 4.33333 3.75878 4.33333 4.33341V23.8334M15.1667 14.0834H17.3333C17.908 14.0834 18.4591 14.3117 18.8654 14.718C19.2717 15.1243 19.5 15.6754 19.5 16.2501V18.4167C19.5 18.9914 19.7283 19.5425 20.1346 19.9488C20.5409 20.3551 21.092 20.5834 21.6667 20.5834C22.2413 20.5834 22.7924 20.3551 23.1987 19.9488C23.6051 19.5425 23.8333 18.9914 23.8333 18.4167V10.6492C23.8336 10.3633 23.7772 10.0802 23.6675 9.8162C23.5578 9.55218 23.3969 9.31248 23.1942 9.11091L19.5 5.41675"
+        stroke="#F5A855"
+        stroke-width="2.16667"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+const ResultBody = () => {
+  return <div className="result-body">
+    <FuelIconSvg />
+    <div className="result-body-content">
+        <p style={{ fontWeight: "bold", fontSize: 15}} >Ooando</p>
+        <p style={{ fontWeight: 500, fontSize: 14}} >600/L</p>
+        <p style={{ fontWeight: 500, fontSize: 12}} >Lagos MainLand Bridge</p>
+    </div>
+  </div>;
 };
