7318aea877af46e7c01dd4cb8f1b13e6e19f62fd
Sheryf Habib <sheryf.534@gmail.com>
2023-11-19 10:08:23 +0100

    add sever error modal

--- frontend/src/components/HomePage/home_page.jsx
+++ frontend/src/components/HomePage/home_page.jsx
@@ -6,10 +6,13 @@ import { ResultSummaryContainer } from "./result_summary";
 import { useState } from "react";
 import Lottie from "lottie-react";
 import animation_data from "../../../src/app_load.json";
+import network_error from "../../../src/networkErr.json";
+import server_error from "../../../src/serverErr.json";
 
 const HomePage = () => {
   const [loading, setLoading] = useState(true);
-  const [networkError, setNetworkError] = useState(true);
+  const [networkError, setNetworkError] = useState(false);
+  const [serverError, setServerError] = useState(true);
   return (
     <div className="main-section">
       {loading && (
@@ -24,7 +27,24 @@ const HomePage = () => {
       {networkError && (
         <div className="main-overlay">
           <div className="network-error-box">
-            
+            <Lottie
+              animationData={network_error}
+              style={{ width: 200 }}
+              loop={true}
+            />
+            <h6>Network Error, Press Ctrl + R to refresh</h6>
+          </div>
+        </div>
+      )}
+      {serverError && (
+        <div className="main-overlay">
+          <div className="network-error-box">
+            <Lottie
+              animationData={server_error}
+              style={{ width: 220 }}
+              loop={true}
+            />
+            <h6>Server Error, Press Ctrl + R to refresh</h6>
           </div>
         </div>
       )}
